aae56c099c0f738ae347570473f71979369159e2
Eldrich Salonga <eldrich.salonga@gmail.com>
2026-03-05 13:40:30 +0800

    add a draw status

--- src/Board.js
+++ src/Board.js
@@ -41,27 +41,80 @@ export default function Board({ xIsNext, squares, onPlay }) {
     }
   }
 
+  const isDraw =
+    !winner &&
+    squares.every((square) => {
+      if (square !== "") {
+        return true;
+      }
+    });
+
+  let status = "";
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else if (isDraw) {
+    status = "It's a Draw";
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
+  }
+
   return (
     <>
       <div className="status">
-        {winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? "X" : "O"}`}
+        <p>{status}</p>
       </div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} win={winArr.includes(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} win={winArr.includes(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} win={winArr.includes(2)}/>
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+          win={winArr.includes(0)}
+        />
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+          win={winArr.includes(1)}
+        />
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+          win={winArr.includes(2)}
+        />
       </div>
 
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} win={winArr.includes(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} win={winArr.includes(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} win={winArr.includes(5)}/>
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+          win={winArr.includes(3)}
+        />
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+          win={winArr.includes(4)}
+        />
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+          win={winArr.includes(5)}
+        />
       </div>
 
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} win={winArr.includes(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} win={winArr.includes(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} win={winArr.includes(8)}/>
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+          win={winArr.includes(6)}
+        />
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+          win={winArr.includes(7)}
+        />
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+          win={winArr.includes(8)}
+        />
       </div>
     </>
   );
